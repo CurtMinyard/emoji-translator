46cf7cb3d112dfafb0eb2e703136973f615e48ea
CurtMinyard <cminyard@perseverenow.org>
2025-06-26 13:42:52 -0500

    finished

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
-testing
+document.getElementById("submit-button").addEventListener("click", function () {
+  const inputText = document.getElementById("translator-input").value.trim();
+  const selectedOption = document.querySelector("input[name='translation-section']:checked").value;
+  const resultsEl = document.getElementById("results");
+
+  resultsEl.innerText = "";
+  if (inputText === "") {
+    resultEl.innerText = "Please enter some text.";
+    return;
+  }
+  const displayResult = (text, func) => {
+    resultsEl.innerText = func(text);
+  };
+
+  switch (selectedOption) {
+    case "encode":
+      displayResult(inputText, encode);
+      break;
+
+    case "translate":
+      displayResult(inputText, translate);
+      break;
+
+    case "madlib":
+      displayResult(inputText, madlib);
+      break;
+
+    case "search":
+      const matches = search(inputText);
+      if (matches.length === 0) {
+        resultsEl.innerText = "No emojis found.";
+      } else {
+        matches.forEach(obj => {
+          const p = document.createElement("p");
+          p.innerText = obj.symbol;
+          results.appendChild(p);
+        });
+      }
+      break;
+
+    case "random":
+      const options = ["encode", "translate", "madlib", "search"];
+      const randIndex = Math.floor(Math.random() * options.length);
+      const randomOption = options[randIndex];
+
+      document.querySelector(`input[value='${randomOption}']`).checked = true;
+      document.getElementById("submit-button").click();
+      break;
+
+    default:
+      resultsEl.innerText = "Unknown translation option selected.";
+  }
+});
+
+
+
+
+
